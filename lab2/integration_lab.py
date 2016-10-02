5d7098ecb6a4a4e5dba78280d801a3ea6758898a
Anne Rogers <amr@cs.uchicago.edu>
2016-10-02 12:41:28 -0500

    Added materials for Lab #2

---
+++ lab2/integration_lab.py
@@ -0,0 +1,16 @@
+def f(x):
+    '''
+    Real valued square function  f(x) == x^2
+    '''
+
+    return x*x
+
+
+def integrate():
+    # Your code here
+    # decide on the number of rectangles
+    # compute the width of the rectangles
+    # use a loop to compute the total area
+    # return the value of totalArea
+    # remove the next line
+    pass
