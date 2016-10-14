a8b16b2ee92438c24bbd16169481476ad1187622
Shrabya Timsina <shrabya@cs.uchicago.edu>
2016-10-13 19:03:26 -0500

    first submission as team

--- pa2/schelling.py
+++ pa2/schelling.py
@@ -41,12 +41,177 @@ def is_satisfied(grid, R, threshold, location):
 
     # We recommend adding an assertion to check that the location does
     # not contain an open (unoccupied) home.
+ 
+    
+    #unpacking location tuple to get info on row and col of home owner
+    (home_row, home_col) = location
+    #finding home owner's colour 
+    homeowner_colour = grid[home_row][home_col]
+   
+    
+    #ensuring we are not checking an empty house
+    if grid[home_row][home_col] == "R" or grid[home_row][home_col] == "B":
+       home_taken = "True"
+    else: 
+        home_taken = "False"
+    assert home_taken
+    
+    blue_num = 0 #num of ne ighbours of blue colour
+    red_num = 0 #num of neighbours of blue colour
+    open_num = 0 #num of open homes in neighbourhood
+    total_neighbour = 0 #total number of homes in the neighbourhood
+    
+    #finding neighbours and their colours around the owner's location
+    for row in range(max(0, home_row - R), \
+        min(home_row + R +1, len(grid))):     #running through rows
 
-    # Remove this comment and the next line, if you choose to use this
-    # function in your decomposition.
-    pass
+        for col in range(max(0, home_col - R), \
+            min(home_col + R +1, len(grid))): #running through columns
 
+            neighbour_formula = abs(home_row - row) + abs(home_col - col)
 
+            #checking if neighbour criteria is fulfilled
+            if neighbour_formula >= 0 and neighbour_formula <= R:
+                total_neighbour = total_neighbour + 1
+                
+                if grid[row][col] == "B":
+                    blue_num = blue_num + 1
+                
+                elif grid[row][col] == "R":
+                    red_num = red_num + 1
+                
+                else:
+                    open_num = open_num + 1
+
+    
+    #checking satisfaction score against threshold
+    if homeowner_colour == "B":
+        satisfaction = blue_num + (0.5 * open_num)
+    else:
+        satisfaction = red_num + (0.5 * open_num)
+
+    if (satisfaction/total_neighbour) >= threshold:
+        return True
+    else:
+        return False
+       
+
+def find_open_locations(grid):
+    '''
+    create and initialize the list of open location
+
+    Inputs:
+        grid: (list of lists of strings) the grid 
+        
+    Returns:
+        available_homes: a list of tuples of location of unoccupied homes
+    '''
+    
+    # initializing list of unoccupied homes
+    available_homes = []
+    
+    num_row = len(grid)
+    num_col = len(grid[0])
+
+    #running loops across rows and then columns of city to find open houses
+    for row in range(0, num_row):
+        for col in range(0, num_col):
+            
+            if grid[row][col] == "O":
+                open_coords = (row, col)
+                available_homes.append(open_coords)
+
+    return available_homes 
+            
+
+def find_dissas_owners(grid, R, threshold):
+    '''
+    create and initialize the list of unsatisfied homeowners
+
+    Inputs:
+        grid: (list of lists of strings) the grid 
+        R: (int) radius for the neighborhood
+        threshold: (float) satisfaction threshold
+
+    Returns:
+        dissas_owners: a list tuples of location of of unsatisfied owners
+    '''
+
+    # initializing list of dissastisfied owners
+    dissas_owners = []
+    
+    num_row = len(grid)
+    num_col = len(grid[0])
+
+    #loops across rows and then columns of city to find dissastisfied owners
+    for row in range(0, num_row):
+        for col in range(0, num_col):
+            if not grid[row][col] == "O":
+                dissas_coords = (row, col)
+                satischeck = is_satisfied(grid, R, threshold, dissas_coords)
+                if satischeck == False:
+                    dissas_owners.append(dissas_coords)
+
+    return dissas_owners
+
+def relocate(grid, R, threshold, available_homes, dissas_owners):
+    '''
+    attempt to relocate an owner to a new location
+    
+    Inputs:
+        grid: (list of lists of strings) the grid 
+        R: (int) radius for the neighborhood
+        threshold: (float) satisfaction threshold
+        available_homes: a list of unoccupied homes
+        dissas_owners: a list of unsatisfied owners
+
+    Returns:
+        grid: list of lists of strings of post-relocation grid
+    '''
+    
+    grid_change = False
+
+    for dis_own in dissas_owners:
+        (d_row, d_col) = dis_own
+        disass_colour = grid[d_row][d_col]
+
+        # Checks if previously unsatisfied homeowners are now satisfied
+        #if false then runs the code below
+        #if true then goes on to next dissastisfied owner
+        satischeck = is_satisfied(grid, R, threshold, dis_own)
+        if satischeck == False:
+
+            for homes in available_homes:
+                (h_row, h_col) = homes
+
+                #occupying tentatively and then check for satistifaction
+                #changing open to new owners colour
+                grid[h_row][h_col] = disass_colour 
+                #changing dissatisfactory place to open
+                grid[d_row][d_col] = "O" 
+                
+                #check for satistifaction after tentative move
+                satischeck = is_satisfied(grid, R, threshold, homes)
+                if satischeck == False:
+                    #still not satisfied so undoing changes to grid
+                    grid[d_row][d_col] = disass_colour #changing back to original colours
+                    grid[h_row][h_col] = "O" # changing available home back to open
+                    #code now loops to next available location
+                    
+                else:
+                    #locking in the move 
+                    #removing new place from open homes list
+                    available_homes.remove(homes)
+                    #adding original place to open homes list
+                    available_homes.insert(0, dis_own)
+                    grid_change = True
+                    break #goes on to the next dissastisfied owner
+
+       
+    
+     
+    return grid_change
+        
 def do_simulation(grid, R, threshold, max_steps):
     '''
     Do a full simulation.
@@ -58,15 +223,33 @@ def do_simulation(grid, R, threshold, max_steps):
         max_steps: (int) maximum number of steps to do
 
     Returns:
-        The function number of steps executed.
+        steps or max_steps : The function number of steps executed.
     '''
     assert utility.is_grid(grid), ("The grid argument has the wrong type.  "
                                    "It should be a list of lists of strings "
                                    "with the same number of rows and columns")
 
-    # YOUR CODE HERE
-    # REPLACE 0 with an appropriate return value
-    return 0
+    available_homes = find_open_locations(grid) #getting list of open homes
+    #getting pre-simulation list of dissastisfied owners
+    dissas_owners = find_dissas_owners(grid, R, threshold) 
+    
+    for steps in range(0, max_steps):
+        #runs one step of the simulation
+        perform_step = relocate(grid, R, threshold, available_homes, dissas_owners)
+          
+        #checks for changes in grid
+        #if true then goes on to next step / if false returns num of steps    
+        if perform_step == False:
+            return steps + 1
+        else:
+            dissas_owners = find_dissas_owners(grid, R, threshold)
+
+    return max_steps
+
+
+
+
+
 
 
 def go(args):
