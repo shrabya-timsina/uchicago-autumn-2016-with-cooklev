18211c2c0c887dad6f0f3fd4aec52c6c669077a1
Anne Rogers <amr@cs.uchicago.edu>
2016-10-14 20:19:59 -0500

    Added PA #3 distribution

---
+++ pa3/basic_algorithms.py
@@ -0,0 +1,51 @@
+# CS121: Analyzing Election Tweets
+# Part 1
+
+from util import sort_count_pairs
+
+def find_top_k(items, k):
+    '''
+    Find the K most frequently occuring items
+
+    Inputs:
+        items: a list of items
+        k: integer 
+
+    Returns: list of K items.
+
+    '''
+    # YOUR CODE HERE
+    # REPLACE RETURN VALUE WITH AN APPROPRIATE VALUE
+    return []
+
+
+def find_min_count(items, min_count):
+    '''
+    Find the items that occur at least min_count times
+
+    Inputs:
+        counts: dictionary that maps items to counts
+        k: integer
+        
+    Returns: sorted list of hashtab/count pairs
+    '''
+
+    # YOUR CODE HERE
+    # REPLACE RETURN VALUE WITH AN APPROPRIATE VALUE
+    return []
+
+
+def find_frequent(items, k):
+    '''
+    Find items where the number of times the item occurs is at least
+    fraction * len(items).
+
+    Input: 
+        items: list of items
+        fraction: float
+
+    Returns: dictionary that maps hashtags (strings) to counts
+    '''
+    # YOUR CODE HERE
+    # REPLACE RETURN VALUE WITH AN APPROPRIATE VALUE
+    return []
