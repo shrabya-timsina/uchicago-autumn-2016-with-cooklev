3305d5fe598e2de89229758b3d46c0b700d04c6e
Anne Rogers <amr@cs.uchicago.edu>
2016-10-16 20:02:04 -0500

    Added distribution for Lab #4

---
+++ lab4/cfpb.py
@@ -0,0 +1,37 @@
+import json
+
+cfpb_16 = [x for x in json.load(open("cfpb16_1000.json"))]
+
+# Task 1
+def count_complaints(complaints, company_name):
+    # Your code goes here
+    # replace 0 with a suitable return value
+    return 0
+
+
+# Task 2
+def find_companies(complaints):
+    # Your code goes here
+    # replace [] with a suitable return value
+    return []
+
+
+# Task 3
+def count_by_state(complaints):
+    # Your code goes here
+    # replace {} with a suitable return value
+    return {}
+
+
+# Task 4
+def count_by_company_by_state(complaints):
+    # Your code goes here
+    # replace {} with a suitable return value
+    return {}
+        
+
+# Task 5
+def complaints_by_company(complaints):
+    # Your code goes here
+    # replace {} with a suitable return value
+    return {}    
