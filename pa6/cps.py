474cdbc715ff09548a9fc68384c9ed6ae15af3ed
Shrabya Timsina <shrabya@cs.uchicago.edu>
2016-11-17 19:59:10 -0600

    commented version

--- pa6/cps.py
+++ pa6/cps.py
@@ -38,9 +38,6 @@ VAR_TO_FILENAME = {GENDER: CODE_TO_FILENAME["gender_code"],
                         ETHNIC: CODE_TO_FILENAME["ethnicity_code"],
                         RACE: CODE_TO_FILENAME["race_code"]}
 
-
-
-
 def build_morg_df(morg_filename):
     '''
     Construct a DF from the specified file.  Resulting dataframe will
@@ -52,34 +49,31 @@ def build_morg_df(morg_filename):
     Returns: pandas dataframe
     '''
 
-    # Your code here...
 
-    # replace None with suitable return value
-
-    
     if os.path.exists(morg_filename) == False:
         return None
       
     morg_df = pd.read_csv(morg_filename)
 
+    # loop through and categorize coded variables 
     for col_name in morg_df.columns[2:6]:
         
         code_file = pd.read_csv(CODE_TO_FILENAME[col_name])
-        categories = code_file[code_file.columns[1]].values
+        categories = code_file[code_file.columns[1]].values 
 
-        if col_name != "ethnicity_code":
+        if col_name != "ethnicity_code": # for codes not starting at 0
            
-            morg_df[col_name] = pd.Categorical.from_codes(morg_df[col_name] - 1, categories)
+            morg_df[col_name] = pd.Categorical.from_codes(\
+                morg_df[col_name] - 1, categories)
 
         else:
             morg_df[col_name] = morg_df[col_name].fillna(value = 0)
-            morg_df[col_name] = pd.Categorical.from_codes(morg_df[col_name], categories)
+            morg_df[col_name] = pd.Categorical.from_codes(\
+                morg_df[col_name], categories)
     
-        
         morg_df.rename(columns = {col_name: col_name[:len(col_name) - 5]}, inplace=True)
 
     return morg_df
-
 
 
 def calculate_weekly_earnings_stats_for_fulltime_workers(df, gender, race, ethnicity):
@@ -108,33 +102,26 @@ def calculate_weekly_earnings_stats_for_fulltime_workers(df, gender, race, ethni
     valid_ethnicity = ["Hispanic", "Non-Hispanic", "All"]
     
    
-
     if (gender not in valid_gender) or (race not in valid_races) \
         or (ethnicity not in valid_ethnicity):
         return (0,0,0,0)
 
-    
-    search_criteria = (df.hours_worked_per_week >= 35) & \
+    search_criteria = (df.hours_worked_per_week >= FULLTIME_MIN_WORKHRS) & \
         (df.employment_status == 'Working')
-
 
     if gender != "All":
         search_criteria = search_criteria & (df.gender == gender)
 
-   
     if race in possible_single_races:
         search_criteria = search_criteria & (df.race == race)
     elif race == "Other":
         search_criteria = search_criteria & \
             (df.race.isin(possible_single_races) == False)
 
-
     if ethnicity == "Hispanic":
         search_criteria = search_criteria & (df.ethnicity != "Non-Hispanic")
     elif ethnicity == "Non-Hispanic":
         search_criteria = search_criteria & (df.ethnicity == "Non-Hispanic")
-
-   
 
     filtered_df = df[search_criteria]
 
@@ -163,31 +150,25 @@ def create_histogram(df, var_of_interest, num_buckets, min_val, max_val):
 
         empty list if num_buckets <= 0 or max_val <= min_val
     '''
-
-    # Your code here...
-
-    # replace [] with a suitable return value
     
     if (num_buckets <= 0) or (max_val <= min_val):
         return []
 
     boundaries = np.linspace(min_val, max_val, num = num_buckets + 1)
-    print(boundaries)
     
-    working_criteria = (df.hours_worked_per_week >= 35) & \
+    working_criteria = (df.hours_worked_per_week >= FULLTIME_MIN_WORKHRS) & \
         (df.employment_status == 'Working')
 
     filtered_df = df[working_criteria]
 
+    #adding bin column to df for var_of_interest 
     filtered_df["bin"] = pd.cut(filtered_df[var_of_interest], 
-                            bins=boundaries,
-                            labels=range(len(boundaries)-1),
-                            include_lowest=True, right=False)
-
+                            bins = boundaries,
+                            labels = range(len(boundaries) - 1),
+                            include_lowest = True, right = False)
 
     bucket_counts_series = (filtered_df["bin"].value_counts().sort_index())
     bucket_counts_list = bucket_counts_series.tolist()
-    print(bucket_counts_list)
 
     return bucket_counts_list
 
@@ -204,62 +185,46 @@ def calculate_unemployment_rates(filenames, age_range, var_of_interest):
 
     Returns: pandas dataframe
     '''
-
-    # Your code here...
-
-    # replace None with suitable return value
-    
-
-    
-
+    (lower_bound, upper_bound) = age_range
     valid_var_of_interest = [GENDER, RACE, ETHNIC]   
 
-    if (var_of_interest not in valid_var_of_interest):
-        return (0,0,0,0)
-    
-    (lower_bound, upper_bound) = age_range
-     
+    if (var_of_interest not in valid_var_of_interest) \
+        or (lower_bound > upper_bound) or (filenames == []):
+        return None
     
     code_file = pd.read_csv(VAR_TO_FILENAME[var_of_interest])
+
+    # extract possible categorical values for var_of_interest
     categories = code_file[code_file.columns[1]].values
+
+    # initialize empty df, to be filled in later
     unemployment_rates_df = pd.DataFrame(index = categories)
     
-
-
-    filenames = set(filenames)
+    filenames = set(filenames) # eliminate duplicate filenames
 
     for dataset in filenames:
 
-        dataset_name = os.path.basename(dataset)
+        dataset_name = os.path.basename(dataset) 
+        year_column_name = dataset_name[6:8] # extracting year from filenames
 
-        year_column_name = dataset_name[6:8]
-
-        
-       
+        # initialize empty series for year columns in unemployment_rates_df,
+        # and fill with 0s
         year_array = np.zeros(shape = len(unemployment_rates_df.index))
-        year_series = pd.Series(data = year_array, index = categories, name = year_column_name)
+        year_series = pd.Series(data = year_array, 
+            index = categories, name = year_column_name)
         
-
         morg_df = build_morg_df(dataset)
-        
         search_criteria = (morg_df.age >= lower_bound) & (morg_df.age <= upper_bound)
-
         morg_df = morg_df[search_criteria]
 
-        
-
         for category in categories:
-            #print(category)
-
             filtered_df = morg_df[morg_df[var_of_interest] == category]
-            #print(filtered_df)
-            
-            num_of_unemployed = (filtered_df[STATUS] == "Layoff").sum() + \
-                (filtered_df[STATUS] == "Looking").sum()
-            #print("unemp", num_of_unemployed)
 
-            total_num_of_individuals = num_of_unemployed + (filtered_df[STATUS] == "Working").sum()
-            #print("unemp", total_num_of_individuals)
+            num_of_unemployed = (filtered_df[STATUS] == "Layoff").sum() + \
+                                (filtered_df[STATUS] == "Looking").sum()
+
+            total_num_of_individuals = num_of_unemployed + \
+                (filtered_df[STATUS] == "Working").sum()
 
             if total_num_of_individuals > 0:
                 unemployment_rate = num_of_unemployed / total_num_of_individuals
@@ -267,38 +232,14 @@ def calculate_unemployment_rates(filenames, age_range, var_of_interest):
             else:
                 unemployment_rate = 0.0
 
-            #print(unemployment_rate)
-
             year_series.set_value(category, unemployment_rate)
-            #print(unemployment_rates_df)
 
         unemployment_rates_df = pd.concat([year_series, unemployment_rates_df], axis=1)
 
-
+    # sort unemployment_rates_df rows in ascending order of index
     unemployment_rates_df = unemployment_rates_df.reindex(unemployment_rates_df.index.astype("str")).sort_index()
+
+    # sort unemployment_rates_df columns in ascending order of year
     unemployment_rates_df.sort_index(axis = 1, inplace = True)
 
     return unemployment_rates_df
-  
-
-"""  
-morg_filename = "data/morg_d10_mini.csv"
-df = build_morg_df(morg_filename)
-gender = "Female"
-race = "WhiteOnly"
-ethnicity = "Non-Hispanic"
-
-
-code_file = pd.read_csv(VAR_TO_FILENAME[RACE])
-categories = code_file[code_file.columns[1]].values
-unemployment_rates = pd.DataFrame(index = categories)
-print(unemployment_rates)
-print(type(unemployment_rates))
-"""
-
-rate_df = calculate_unemployment_rates(["data/morg_d14.csv", "data/morg_d10.csv", "data/morg_d07.csv"], (50, 70), GENDER)
-print(rate_df)
-
-print()
-rate_df = calculate_unemployment_rates(["data/morg_d14.csv", "data/morg_d10.csv", "data/morg_d07.csv"], (22, 29), GENDER)
-print(rate_df)
